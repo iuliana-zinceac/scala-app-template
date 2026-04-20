fa64caaa3ced42d3ff5f69003b22c4ac4c07e591
Scala Steward <scala-steward@email.com>
2026-04-20 09:48:46 +0000

    Update flyway-core, ... to 12.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Versions {
   val logbackEncoder = "9.0"
   val http4s = "0.23.33"
   val tapir = "1.13.14"
-  val flyway = "11.20.3"
+  val flyway = "12.4.0"
   val doobie = "1.0.0-RC12"
   val postgres = "42.7.10"
 
